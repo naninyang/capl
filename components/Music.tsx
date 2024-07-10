a52b0af83423f705e0b1f50139a3fcbbbbc6ae36
Chloe <gtalk.with.nani@gmail.com>
2024-07-10 17:49:10 +0900

    Layout fixed

--- components/Music.tsx
+++ components/Music.tsx
@@ -1,20 +1,23 @@
 import styles from "@/styles/Music.module.sass";
 import { MusicIcon } from "./Icons";
+import Anchor from "./Anchor";
 export default function Music() {
   return (
     <div className={`${styles.music} ${styles.day}`}>
       <div className={styles["music-player"]}>뮤직 플레이어</div>
       <div className={styles["music-bar"]}>
         <div className={styles.started}>
-          <div className={styles["music-info"]}>
-            <div className={styles.thumbnail}>
-              <MusicIcon />
+          <button type='button'>
+            <div className={styles["music-info"]}>
+              <div className={styles.thumbnail}>
+                <MusicIcon />
+              </div>
+              <div className={styles.info}>
+                <strong>어떤 노래를 듣고 싶으세요?</strong>
+                <cite>밤과 어울리는 곡을 추천해 드려요</cite>
+              </div>
             </div>
-            <div className={styles.info}>
-              <strong>어떤 노래를 듣고 싶으세요?</strong>
-              <cite>밤과 어울리는 곡을 추천해 드려요</cite>
-            </div>
-          </div>
+          </button>
         </div>
       </div>
     </div>
